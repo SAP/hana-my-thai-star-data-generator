d6a087e97cf8853e63bff5a2d7089a8e583237b6
Jonathan Bregler <jonathan.bregler@sap.com>
2020-08-28 10:46:59 +0200

    Exclude all default users when importing the user data

--- src/main.py
+++ src/main.py
@@ -67,7 +67,7 @@ def clean_up():
 
     for table in tables:
         if table == '"USER"':
-            query ="""delete from "USER" where "USERNAME" NOT IN ('manager', 'waiter', 'user0')"""
+            query ="""delete from "USER" where "USERNAME" NOT IN ('manager', 'waiter', 'user0', 'cook', 'barkeeper', 'chief')"""
             cursor.execute(query)
         else:
             cursor.execute('delete from {}'.format(table))
